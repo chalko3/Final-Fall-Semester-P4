2bc0ee980a1a5d3a364426d9b62631ac952c8f7c
chalko3 <113392114+chalko3@users.noreply.github.com>
2023-12-18 12:34:55 -0800

    Added New Scripts and Managers
    Added Scripts: Game Manager, SpawnManager, UIManager, and Zombie.
    
    Updated Code: Coin and PlayerController.
    
    Added Text and Button.
    
    Added Objects: Game Manager, Spawn Manager.
    
    Audio Added: Coin Sound and Knife Slice.

---
+++ Final mini game/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public int coinAmount = 10;
+
+    public float areaRange = 45f;
+    public GameObject collectibleObject;
+    public GameObject enemyObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRandomAmountOfEnemies());
+        SpawnCollectibleObject();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnEnemyObject()
+    {
+        Instantiate(enemyObject, RandomSpawnPosition(), enemyObject.transform.rotation);
+    }
+
+    public void SpawnCollectibleObject()
+    {
+        for (int i = 0; i < coinAmount; i++)
+        {
+            Instantiate(collectibleObject, RandomSpawnPosition(), collectibleObject.transform.rotation);
+        }
+    }
+
+    Vector3 RandomSpawnPosition()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(-areaRange, areaRange), 1f, Random.Range(-areaRange, areaRange));
+
+        return spawnPosition;
+    }
+
+    IEnumerator SpawnRandomAmountOfEnemies()
+    {
+        while(true)
+        {
+            float randomSeconds = Random.Range(1, 8);
+
+            yield return new WaitForSeconds(randomSeconds);
+
+            int numberOfEnemies = Random.Range(1, 3);
+
+            for (int i = 0; i < numberOfEnemies; i++)
+            {
+                Instantiate(enemyObject, RandomSpawnPosition(), enemyObject.transform.rotation);
+            }
+        }
+    }
+}
